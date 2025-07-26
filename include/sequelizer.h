ba7edcedf4ad5c1f59f3e771394ae703745fc3ea
smagiero <smagiero@users.noreply.github.com>
2025-07-25 23:29:05 -0400

    Add MIT license and initial public API header

---
+++ include/sequelizer.h
@@ -0,0 +1,22 @@
+#ifndef SEQUELIZER_H
+#define SEQUELIZER_H
+
+// Sequelizer - Open Source DNA Sequence Analysis Toolkit
+// Public API definitions
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Version information
+#define SEQUELIZER_VERSION_MAJOR 0
+#define SEQUELIZER_VERSION_MINOR 1
+#define SEQUELIZER_VERSION_PATCH 0
+
+// Function declarations will go here as we develop the public API
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SEQUELIZER_H
