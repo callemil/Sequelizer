5c390360aeeeee902a009d5002900ef246ea3481
smagiero <smagiero@users.noreply.github.com>
2025-07-25 23:07:41 -0400

    Initial sequelizer public interface

---
+++ src/sequelizer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Sequelizer - Open Source DNA Sequence Analysis\n");
+    printf("Basic functionality coming soon...\n");
+    return 0;
+}
